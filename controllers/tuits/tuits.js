2c4fca113f86eb5f8b50c9358b2be71667f4e341
Jack Furci <furci.j@northeastern.edu>
2023-04-02 22:48:55 -0400

    first commit

---
+++ controllers/tuits/tuits.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        "_id": "123", "topic": "Space", "username": "SpaceX",
+        "handle": "@spacex", "time": "2h", 
+        "image": "https://infosec-jobs.com/media/company/logo/20/03/spacex.jpg",
+        "title": "SpaceX's Mission",
+        "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
+        "liked": true, "disliked": false, "likes": 2345, "dislikes": 123, "replies": 123, "retuits": 432
+    },
+    {
+        "_id": "234", "topic": "Traffic", "username": "The Boring Company",
+        "handle": "@boringcompany", "time": "2h", 
+        "image": "https://i.pcmag.com/imagery/articles/04e4rbdlYClonRk2T3FiJXN-1..v1569492116.jpg",
+        "title": "The Boring Company fixing traffic",
+        "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
+        "liked": true, "disliked": false, "likes": 2345, "dislikes": 456, "replies": 321, "retuits": 432
+    },
+    {
+        "_id": "345", "topic": "Brain Injuries", "username": "Neurolink",
+        "handle": "@neurolink", "time": "3h", 
+        "image": "https://img1.cgtrader.com/items/2590505/c0e98afb50/large/neuralink-logo-v1-004-3d-model-c0e98afb50.jpg",
+        "title": "Neurolink fixes paralisis and blindness",
+        "tuit": "Neurolink is an American company founded by Elon Musk that specializes in brain to computer integration. The first application will be to cure spinal cord injuries so that paraplegics can walk again. Other applications include curing blindness and mental disorders.",
+        "liked": true, "disliked": false, "likes": 3456, "dislikes": 654, "replies": 345, "retuits": 654
+    },
+];
